01ed67635c701efbfe6c6273bd8cb79b1406d0a3
adwaithks <adwaith2507@gmail.com>
2021-05-11 11:45:02 +0530

    .env

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,16 +1,12 @@
 import mongoose from 'mongoose';
+require('mongoose').config();
 
 const initDB = () => {
     if (mongoose.connections[0].readyState){
         console.log('Already Connected')
         return
     }
-
-<<<<<<< HEAD
-    mongoose.connect('mongodb+srv://ruby:ruby@cluster0.pfsz5.mongodb.net/Cluster0?retryWrites=true&w=majority', {
-=======
-    mongoose.connect('', {
->>>>>>> ae723b891441398f49005637ddfa055c7d352314
+    mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(() => {
